78d66766225f875aa32e8174e6f3a17411b39fcb
kyy08 <kyyelzz1@gmail.com>
2023-04-03 11:38:42 +0700

    membuat struktur mahasiswa

--- ActivityStructure_101/ActivityStructure_101.cpp
+++ ActivityStructure_101/ActivityStructure_101.cpp
@@ -8,33 +8,3 @@ struct mahasiswa
 	string alamat;
 	int umur;
 };
-
-
-int main() {
-	mahasiswa mhs1, mhs2;
-
-	mhs1.NIM = "20210140019";
-	mhs1.nama = "Ardian Mulya";
-	mhs1.alamat = "Wonosobo";
-	mhs1.umur = 20;
-
-	cout << "Masukan NIM :";
-	cin >> mhs2.NIM;
-	cout << "Masukan Nama :";
-	cin >> mhs2.nama;
-	cout << " Masukan Alamat :";
-	cin >> mhs2.alamat;
-	cout << "Masukan Umur :";
-	cin >> mhs2.umur;
-
-	cout << "\nNIM = " << mhs1.NIM;
-	cout << "\nNama = " << mhs1.nama;
-	cout << "\nAlamat = " << mhs1.alamat;
-	cout << "\nUmur = " << mhs1.umur;
-
-	cout << "\n\nNIM = " << mhs2.NIM;
-	cout << "\n\nNama = " << mhs2.nama;
-	cout << "\n\nAlamat = " << mhs2.alamat;
-	cout << "\n\nUmur = " << mhs2.umur;
-
-}
